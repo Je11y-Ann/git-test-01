4ce0049d20035087f2b3790906b0722078940111
Je11y <1214650391@qq.com>
2021-05-30 20:38:56 +0800

    first commit

---
+++ src/main/java/com/je11y/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.je11y.git;
+
+/**
+ * @Author: Je11y
+ * @Date: 2021/5/30 20:33
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+    }
+}
